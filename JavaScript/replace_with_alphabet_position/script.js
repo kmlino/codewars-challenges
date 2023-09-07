541be1df84ae2866a6882b180da39795b8784c7e
kmlino <kmlino@outlook.com>
2023-09-07 19:46:59 -0300

    New challenge

---
+++ JavaScript/replace_with_alphabet_position/script.js
@@ -0,0 +1,13 @@
+function alphabetPosition(text) {
+    res = '';
+    for (var i = 0; i < text.length; i++) {
+      for (var j = 65; j <= 90; j++) {
+        if (text[i].codePointAt(j) < 65 || text[i].codePointAt(j) > 90) continue
+        if (text[i].toUpperCase() == String.fromCharCode(j)) {
+          res += !res ? `${j-64}` : ` ${j-64}`;
+          console.log(String.fromCharCode(j))
+        }
+      }
+    }
+    return res
+}
