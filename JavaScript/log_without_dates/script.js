003294110f526010602af7b579f4e95ebde88222
kmlino <kmlino@outlook.com>
2023-09-07 19:34:03 -0300

    Last refactoring

--- JavaScript/log_without_dates/script.js
+++ JavaScript/log_without_dates/script.js
@@ -2,28 +2,25 @@ function checkLogs(log) {
   if (log.length == 0) return 0
   if (log.length == 1) return 1
   
-
   let i;
   let cont = 1;
 
-  for (i = 0; i < log.length; i++) {
-    if (i > 0) {
-      let [prevHr, prevMn, prevSc] = log[i-1].split(':')
-      let [currHr, currMn, currSc] = log[i].split(':')
-      
-      if (prevHr === currHr && prevMn === currMn && prevSc === currSc) {
-        cont++;
-      }
-      else if (prevHr === currHr && prevMn === currMn && prevSc > currSc) {
-        cont++;
-      }
-      else if (prevHr === currHr && prevMn > currMn) {
-        cont++;
-      }
-      else if (prevHr > currHr) {
-        cont++;
-      }
+  for (i = 1; i < log.length; i++) {
+    let [prevHr, prevMn, prevSc] = log[i-1].split(':')
+    let [currHr, currMn, currSc] = log[i].split(':')
+
+    if (prevHr === currHr && prevMn === currMn && prevSc === currSc) {
+      cont++;
+    }
+    else if (prevHr === currHr && prevMn === currMn && prevSc > currSc) {
+      cont++;
+    }
+    else if (prevHr === currHr && prevMn > currMn) {
+      cont++;
+    }
+    else if (prevHr > currHr) {
+      cont++;
     }
   }
-    return cont;
+  return cont;
 }
